ed9bfa5b3f2959f313b4464903c05d1dd1f47df9
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-04-22 15:51:27 -0700

    fixing the proto and the handler

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,35 +2,20 @@ import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 import * as assembly from "./pb/assembly";
 import { Contract } from "../generated/schema";
 
-export function handleContract(bytes: ArrayBuffer): void {
-    log.info("Handling contract: {}", [bytes.toString()]);
-    const contract = assembly.example.Contract.decode(bytes);
-
-    if (contract.address == null) {
+export function handleContract(bytes: Uint8Array): void {
+    let contracts = assembly.example.Contracts.decode(bytes.buffer);
+    if (contracts.contracts.length == 0) {
+        log.error("No contracts found", []);
         return;
     } else {
-        log.debug("Contract: {}", [contract.address.toString()]);
-
-        if (Contract.load(contract.address) != null) {
-            log.warning("Contract {} already exists", [
-                contract.address.toString(),
-            ]);
-            return;
-        } else {
-            log.info("Creating entity for contract {}", [
-                contract.address.toString(),
-            ]);
-            const contractEntity = new Contract(contract.address);
-
-            log.info("Block number: {}", [contract.blockNumber.toString()]);
-            log.info("Timestamp: {}", [contract.timestamp.toString()]);
-
-            contractEntity.id = Bytes.fromHexString(contract.address).toHex();
-            contractEntity.blockNumber = BigInt.fromU64(
-                contract.blockNumber as u64
-            );
-            contractEntity.timestamp = contract.timestamp.toString();
-            contractEntity.save();
+        // Loop through all contracts
+        for (let i = 0; i < contracts.contracts.length; i++) {
+            let contractInfo = contracts.contracts[i];
+            let entity = new Contract(contractInfo.address.toString());
+            entity.id = contractInfo.address.toString();
+            entity.timestamp = contractInfo.timestamp;
+            entity.blockNumber = BigInt.fromU64(contractInfo.blockNumber);
+            entity.save();
         }
     }
 }
